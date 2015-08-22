926d9369d8c69eabcdfdc3c567cc8d6c01e474ed
PJ Dietz <pjdietz@gmail.com>
2015-08-22 13:23:40 -0400

    Ensure StringParser does not set body when the request has no body

--- test/stringParser.js
+++ test/stringParser.js
@@ -14,47 +14,34 @@ var assert = require("chai").assert,
 describe("String Parser", function () {
 
     it("Reads first non-empty line as reqest line", function () {
-
         var parser, requestLine;
-
         parser = new StringParser();
         parser.on("requestLine", function (line) {
             requestLine = line;
         });
         parser.parse(request);
-
         requestLine.should.equal("POST / HTTP/1.1");
-
     });
 
     it("Reads lines following request line as headers", function () {
-
         var parser, headers;
-
         headers = [];
-
         parser = new StringParser();
         parser.on("header", function (line) {
             headers.push(line);
         });
         parser.parse(request);
-
         headers.length.should.equal(2);
-
     });
 
     it("Receives body as stream", function () {
-
         var parser, bodyStream, bodyString;
-
         parser = new StringParser();
         parser.on("body", function (stream) {
             bodyStream = stream;
         });
         parser.parse(request);
-
         assert(bodyStream !== undefined);
-
         bodyString = bodyStream.read().toString();
         bodyString.should.equal("Here is the body");
     });
@@ -68,6 +55,27 @@ describe("String Parser", function () {
         });
         parser.parse(request);
         assert(ended);
+    });
+
+    it("Does not receieve body for request with no body", function () {
+        var parser, request, body, ended;
+        body = false;
+        ended = false;
+        request = [
+            "GET / HTTP/1.1",
+            "Host: localhost",
+            "Content-type: application/json",
+        ].join("\n");
+        parser = new StringParser();
+        parser.on("body", function () {
+            body = true;
+        });
+        parser.on("end", function () {
+            ended = true;
+        });
+        parser.parse(request);
+        assert(ended);
+        assert(!body);
     });
 
 });
