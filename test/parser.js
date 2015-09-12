ec5c7e3b9f220859e8de6296d398638f418273a8
PJ Dietz <pjdietz@gmail.com>
2015-09-12 17:50:48 -0400

    Begin rewriting Parser to absorb StringParser

--- test/parser.js
+++ test/parser.js
@@ -2,10 +2,9 @@
 "use strict";
 
 var assert = require("chai").assert,
-    should = require("chai").should(),
-    EventEmitter = require("events").EventEmitter,
-    Parser = require("../lib/parser").Parser,
-    url = require("url");
+    expect = require("chai").expect;
+
+var Parser = require("../lib/parser").Parser;
 
 describe("Parser", function () {
 
@@ -18,231 +17,42 @@ describe("Parser", function () {
 
     describe("Request Line", function () {
 
-        describe("Method", function () {
+        var requests = [
+            {
+                description: "No method, full URI",
+                request: "http://localhost/dogs",
+                method: "GET",
+                host: "localhost",
+                path: "/dogs"
+            },
+            {
+                description: "Method, path only",
+                request: "POST /cats",
+                method: "POST",
+                host: "localhost",
+                path: "/cats"
+            },
+            {
+                description: "Blank lines at start",
+                request: ["", "   ", "PUT /hamsters"].join("\n"),
+                method: "PUT",
+                host: "localhost",
+                path: "/hamsters"
+            }
+        ];
 
-            var tests = [
-                {
-                    expected: "GET",
-                    requestLine: "/cat/molly",
-                    description: "Assumes GET for one-word line"
-                },
-                {
-                    expected: "DELETE",
-                    requestLine: "DELETE /delete/me",
-                    description: "Reads method from two-word line"
-                },
-                {
-                    expected: "POST",
-                    requestLine: "POST /cat/molly HTTP/1.1",
-                    description: "Reads method from three--or-more-word line"
-                }
-            ];
-            tests.forEach(function (test) {
-                it(test.description, function () {
-                    var parser = new Parser(),
-                        parsedRequestOptions;
-
-                    parser.stringParser = new EventEmitter();
-                    parser.stringParser.parse = function () {
-                        this.emit("requestLine", test.requestLine);
-                        this.emit("end");
-                    };
-
-                    parser.parse("", null, function (rqst, conf) {
-                        parsedRequestOptions = rqst;
+        describe("Parses method", function () {
+            requests.forEach(function (test) {
+                it(test.description, function (done) {
+                    var parser = new Parser();
+                    parser.parse(test.request, function (error, options, body) {
+                        expect(options.method).to.equal(test.method);
+                        done();
                     });
-
-                    parsedRequestOptions.method.should.equal(test.expected);
-                });
-            });
-
-        });
-
-        describe("Path", function () {
-
-            var tests = [
-                {
-                    expected: "/cats/molly",
-                    requestLine: "/cats/molly",
-                    description: "Reads line as path for one-word line"
-                },
-                {
-                    expected: "/delete/me",
-                    requestLine: "DELETE /delete/me",
-                    description: "Reads second word from two-word line"
-                },
-                {
-                    expected: "/cats/molly",
-                    requestLine: "GET http://localhost/cats/molly HTTP/1.1",
-                    description: "Reads path from URI"
-                },
-                {
-                    expected: "/",
-                    requestLine: "GET http://localhost",
-                    description: "Defaults to / when no path is present"
-                }
-            ];
-            tests.forEach(function (test) {
-                it(test.description, function () {
-                    var parser = new Parser(),
-                        parsedRequestOptions;
-
-                    parser.stringParser = new EventEmitter();
-                    parser.stringParser.parse = function () {
-                        this.emit("requestLine", test.requestLine);
-                        this.emit("end");
-                    };
-
-                    parser.parse("", null, function (rqst, conf) {
-                        parsedRequestOptions = rqst;
-                    });
-
-                    parsedRequestOptions.path.should.equal(test.expected);
-                });
-            });
-        });
-
-        describe("Host and port", function () {
-
-            var tests = [
-                {
-                    expectedHost: "localhost",
-                    expectedHostname: "localhost",
-                    expectedPost: undefined,
-                    requestLine: "GET http://localhost/cats/molly HTTP/1.1",
-                    description: "Reads host from URI"
-                },
-                {
-                    expectedHost: "localhost:8080",
-                    expectedHostname: "localhost",
-                    expectedPost: "8080",
-                    requestLine: "GET http://localhost:8080/cats/molly HTTP/1.1",
-                    description: "Reads host with port from URI"
-                },
-                {
-                    expectedHost: undefined,
-                    expectedHostname: undefined,
-                    expectedPost: undefined,
-                    requestLine: "GET /cats/molly HTTP/1.1",
-                    description: "Does not include host when not set in request line"
-                }
-            ];
-            tests.forEach(function (test) {
-                it(test.description, function () {
-                    var parser = new Parser(),
-                        parsedRequestOptions;
-
-                    parser.stringParser = new EventEmitter();
-                    parser.stringParser.parse = function () {
-                        this.emit("requestLine", test.requestLine);
-                        this.emit("end");
-                    };
-
-                    parser.parse("", null, function (rqst, conf) {
-                        parsedRequestOptions = rqst;
-                    });
-
-                    assert(parsedRequestOptions.host === test.expectedHost);
-                    assert(parsedRequestOptions.hostname === test.expectedHostname);
                 });
             });
         });
 
     });
 
-    describe("Header Lines", function () {
-
-        var stringParser;
-
-        stringParser = new EventEmitter();
-        stringParser.parse = function () {
-            this.emit("requestLine", "/");
-            this.emit("header", "Cache-control: no-cache");
-            this.emit("header", "Content-type: text/plain");
-            this.emit("header", "?cat=Molly");
-            this.emit("header", "&dog:Bear");
-            this.emit("header", "  & hamster: Fizzgig the Hammie");
-            this.emit("header", "  @port=8080");
-            this.emit("header", "@protocol: https");
-            this.emit("header", "?noop");
-            this.emit("header", "@noop");
-            this.emit("header", "@colon:before=");
-            this.emit("header", "# This is a comment");
-            this.emit("end");
-        };
-
-        describe("Headers", function () {
-            it("Parses headers", function () {
-                var parser = new Parser(), parsedRequestOptions;
-                parser.stringParser = stringParser;
-                parser.parse("", null, function (rqst, conf) {
-                    parsedRequestOptions = rqst;
-                });
-                parsedRequestOptions.headers["Cache-control"].should.equal("no-cache");
-                parsedRequestOptions.headers["Content-type"].should.equal("text/plain");
-            });
-            it("Does not parse non-headers as headers", function () {
-                var parser, parsedRequestOptions, count, headers, property;
-                parser = new Parser();
-                parser.stringParser = stringParser;
-                parser.parse("", null, function (rqst, conf) {
-                    parsedRequestOptions = rqst;
-                });
-                count = 0;
-                headers = parsedRequestOptions.headers;
-                for (property in headers) {
-                    if (headers.hasOwnProperty(property)) {
-                       ++count;
-                    }
-                }
-                count.should.equal(2);
-            });
-        });
-
-        describe("Configuration", function () {
-            it("Parses confguration options with : separator", function () {
-                var parser = new Parser(),
-                    parsedRequestOptions;
-                parser.stringParser = stringParser;
-                parser.parse("", null, function (rqst) {
-                    parsedRequestOptions = rqst;
-                });
-                parsedRequestOptions.protocol.should.equal("https");
-            });
-            it("Parses confguration options with = separator", function () {
-                var parser = new Parser(),
-                    parsedRequestOptions;
-                parser.stringParser = stringParser;
-                parser.parse("", null, function (rqst) {
-                    parsedRequestOptions = rqst;
-                });
-                parsedRequestOptions.port.should.equal("8080");
-            });
-        });
-
-        describe("Query", function () {
-            var parser, request, query;
-            parser = new Parser();
-            parser.stringParser = stringParser;
-            parser.parse("", null, function (rqst, conf) {
-                request = rqst;
-            });
-            query = url.parse(request.path, true).query;
-            it("Parses query parameters beginning with ?", function () {
-                query.cat.should.equal("Molly");
-            });
-            it("Parses query parameters beginning with &", function () {
-                query.dog.should.equal("Bear");
-            });
-            it("Parses query parameters with = separator", function () {
-                query.cat.should.equal("Molly");
-            });
-            it("Parses query parameters with : separator", function () {
-                query.dog.should.equal("Bear");
-            });
-            it("Parses query parameters with space after starting character", function () {
-                query.hamster.should.equal("Fizzgig the Hammie");
-            });
-        });
-    });
 });
