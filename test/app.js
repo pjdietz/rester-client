b34572acc2dc7669ed1570eae700c3b7e07c55a0
PJ Dietz <pjdietz@gmail.com>
2015-09-23 16:19:58 -0400

    App reads request from stdin or first argument

--- test/app.js
+++ test/app.js
@@ -9,6 +9,9 @@ var assert = require("chai").assert,
     sinon = require("sinon"),
     sinonChai = require("sinon-chai");
 
+chai.should();
+chai.use(sinonChai);
+
 var App = require("../lib/app").App;
 
 function stringToStream(string) {
@@ -18,28 +21,69 @@ function stringToStream(string) {
     return s;
 }
 
+function createApp(stdin, argv) {
+    var app = new App();
+    app._input = stringToStream(stdin || "");
+    app._argv = argv || [];
+    return app;
+}
+
 describe("App", function () {
-    it("Creates instance", function () {
+    it("Creates instance with default options", function () {
         var app = new App();
         assert.isDefined(app);
     });
 
     describe("Initial input", function () {
 
-        it("Parses request from stdin", function (done) {
+        it("Parses request from first argument.", function (done) {
             var app,
                 inputString = "GET /path HTTP/1.1\nHost: localhost",
                 parse;
-            app = new App();
-            app._input = stringToStream(inputString);
+            app = createApp("", [inputString]);
             parse = sinon.spy(app._parser, "parse");
             app.on("end", function () {
-                assert(parse.calledWith(inputString));
+                parse.should.have.been.calledWith(inputString);
                 done();
             });
             app.run();
         });
 
+        it("Parses request from stdin", function (done) {
+            var app,
+                inputString = "GET /path HTTP/1.1\nHost: localhost",
+                parse;
+            app = createApp(inputString, []);
+            parse = sinon.spy(app._parser, "parse");
+            app.on("end", function () {
+                parse.should.have.been.calledWith(inputString);
+                done();
+            });
+            app.run();
+        });
+
+        it("Stdin supercedes arguments", function (done) {
+            var app,
+                inputString1 = "GET /path HTTP/1.1\nHost: localhost",
+                inputString2 = "GET /other/path HTTP/1.1\nHost: localhost",
+                parse;
+            app = createApp(inputString1, [inputString2]);
+            parse = sinon.spy(app._parser, "parse");
+            app.on("end", function () {
+                parse.should.have.been.calledWith(inputString1);
+                done();
+            });
+            app.run();
+        });
+
+        it("Emits error when no request is present", function (done) {
+            var app;
+            app = createApp();
+            app.on("error", function () {
+                done();
+            });
+            app.run();
+        });
     });
 
 });
