98e0d6bd0ba71e4595ddc75586586359a66ac670
PJ Dietz <pjdietz@gmail.com>
2015-10-03 14:07:24 -0400

    Add index file
    Fix typo in index
    
    Fix paths in index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var App = require("./lib/app").App,
+    Client = require("./lib/client").Client,
+    Parser = require("./lib/parser").Parser;
+
+exports.App = App;
+exports.Client = Client;
+exports.Parser = Parser;
