2ef3c1adf0b66e87ceaf67c0285648d7caeb33c2
PJ Dietz <pjdietz@gmail.com>
2016-02-26 20:55:35 -0500

    Transaction can perform rudimentary redirects

--- src/transaction.js
+++ src/transaction.js
@@ -9,37 +9,82 @@ function Transaction(requestOptions, requestBody, configuration) {
     EventEmitter.call(this);
     this.requestOptions = requestOptions;
     this.requestBody = requestBody;
-    this.configuration = configuration;
+    this.configuration = configuration || {};
     this.requests = [];
     this.responses = [];
     this.requestFormatter = new RequestFormatter();
     this.responseFormatter = new ResponseFormatter();
+    // Things that could move into a state.
+    this.redirectCount = 0;
+    this.currentRequestOptions = null;
 }
 
 util.inherits(Transaction, EventEmitter);
 
 Transaction.prototype.send = function () {
+    this.sendRequest(this.requestOptions, this.requestBody);
+    this.emit('request');
+};
+
+Transaction.prototype.sendRequest = function (requestOptions, body) {
     var _this = this;
-
-    this.requests.push(this.requestFormatter.format(
-        this.requestOptions,
-        this.requestBody
-    ));
-
-    var request = http.request(this.requestOptions, function (response) {
-        var body = '';
-        response.setEncoding('utf8');
-        response.on('data', function (chunk) {
-            body += chunk;
-        });
-        response.on('end', function () {
-            _this.emit('response');
-            _this.emit('end');
-            _this.responses.push(_this.responseFormatter.format(response, body));
-        });
+    this.currentRequestOptions = requestOptions;
+    this.requests.push(this.requestFormatter.format(requestOptions, body));
+    var request = http.request(requestOptions, function (response) {
+        _this.onResponse(response);
     });
     request.end();
-    this.emit('request');
+};
+
+Transaction.prototype.onResponse = function (response) {
+    var _this = this,
+        body = '';
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+        body += chunk;
+    });
+    response.on('end', function () {
+        _this.completeResponse(response, body);
+    });
+};
+
+Transaction.prototype.completeResponse = function (response, body) {
+    this.emit('response');
+    this.responses.push(this.responseFormatter.format(response, body));
+    if (this.shouldRedirect(response)) {
+        this.tryToRedirect(response);
+    } else {
+        this.emit('end');
+    }
+};
+
+Transaction.prototype.shouldRedirect = function (response) {
+    var redirectCodes = this.configuration.redirectStatusCodes || [];
+    for (var i = 0; i < redirectCodes.length; ++i) {
+        if (response.statusCode === redirectCodes[i]) {
+            return true;
+        }
+    }
+    return false;
+};
+
+Transaction.prototype.tryToRedirect = function (response) {
+
+    // TODO Need to parse the location. If it is relative, we need to reference
+    // the request used to get this response.
+
+    var previous = this.currentRequestOptions;
+
+    var request = {
+        method: 'GET',
+        hostname: previous.hostname,
+        port: previous.port,
+        path: response.headers.location
+    };
+
+    this.sendRequest(request);
+    this.redirectCount += 1;
+    this.emit('redirect');
 };
 
 Transaction.prototype.getRequest = function () {
@@ -75,6 +120,5 @@ ResponseFormatter.prototype.format = function (response, body) {
 };
 
 // -----------------------------------------------------------------------------
-
 
 module.exports = Transaction;
--- test/tests/unit/transaction.js
+++ test/tests/unit/transaction.js
@@ -14,7 +14,13 @@ chai.use(sinonChai);
 describe('Transaction', function () {
 
     var port = 8761,
-        server;
+        server,
+        requestListener,
+        redirectListener,
+        responseListener,
+        endListener,
+        errorListener,
+        transaction;
 
     before(function () {
         server = http.createServer();
@@ -25,6 +31,21 @@ describe('Transaction', function () {
                 response.setHeader('Content-Type', 'text/plain');
                 response.write('Hello, world!');
                 response.end();
+            } else if (request.url.startsWith('/redirect/')) {
+                // Redirect the client from /redirect/{code}/{n} to
+                // /redirect/{code}/{n-1}; If n = 1, redirects to /hello
+                (function () {
+                    var parts, code, n, location = '/hello';
+                    parts = request.url.slice(1).split('/');
+                    code = parts[1];
+                    n = parseInt(parts[2], 10);
+                    if (n > 1) {
+                        location = '/redirect/' + code + '/' + (n - 1);
+                    }
+                    response.statusCode = code;
+                    response.setHeader('Location', location);
+                    response.end();
+                })();
             } else {
                 response.statusCode = 404;
                 response.setHeader('Content-Type', 'text/plain');
@@ -40,22 +61,29 @@ describe('Transaction', function () {
     });
 
     beforeEach(function () {
-
+        requestListener = sinon.spy();
+        redirectListener = sinon.spy();
+        responseListener = sinon.spy();
+        endListener = sinon.spy();
+        errorListener = sinon.spy();
     });
 
-    function createHelloTransaction() {
-        return new Transaction({
-            protocol: 'http:',
-            hostname: 'localhost',
-            port: port,
-            method: 'GET',
-            path: '/hello'
-        });
+    afterEach(function () {
+        transaction.removeAllListeners();
+    });
+
+    function addListeners() {
+        transaction.on('request', requestListener);
+        transaction.on('redirect', redirectListener);
+        transaction.on('response', responseListener);
+        transaction.on('end', endListener);
+        transaction.on('error', errorListener);
     }
 
-    context('Request with no body or redirection', function () {
-        var transaction;
-        beforeEach(function () {
+    // -------------------------------------------------------------------------
+
+    context('Request with no redirection', function () {
+        beforeEach(function (done) {
             transaction = new Transaction({
                 protocol: 'http:',
                 hostname: 'localhost',
@@ -63,55 +91,72 @@ describe('Transaction', function () {
                 method: 'GET',
                 path: '/hello'
             });
+            addListeners();
+            transaction.send();
+            setTimeout(done, 10);
         });
-        context('When request is sent', function () {
-            var listener = sinon.spy();
-            beforeEach(function () {
-                transaction.on('request', listener);
-                transaction.send();
+        describe('Events', function () {
+            it('Emits "request" once', function () {
+                expect(requestListener).calledOnce;
             });
-            it('Emits "request" event', function (done) {
-                setTimeout(function () {
-                    expect(listener).called;
-                    done();
-                }, 10);
+            it('Emits "response" once', function () {
+                expect(responseListener).calledOnce;
             });
-            it('Provides request as string', function (done) {
-                setTimeout(function () {
-                    expect(transaction.getRequest()).to.contain('GET /hello HTTP/1.1');
-                    done();
-                }, 10);
+            it('Emits "end" once', function () {
+                expect(endListener).calledOnce;
+            });
+            it('Does not emits "redirect"', function () {
+                expect(redirectListener).not.called;
+            });
+            it('Does not emit "error"', function () {
+                expect(errorListener).not.called;
             });
         });
-        context('When response is receieved', function () {
-            var responseListener,
-                endListener;
-            beforeEach(function () {
-                responseListener = sinon.spy();
-                endListener = sinon.spy();
-                transaction.on('response', responseListener);
-                transaction.on('response', endListener);
-                transaction.send();
+        describe('Message', function () {
+            it('Provides request as string', function () {
+                expect(transaction.getRequest()).to.contain('GET /hello HTTP/1.1');
+            });
+            it('Provides response as string', function () {
+                expect(transaction.getResponse()).to.contain('HTTP/1.1 200 OK');
+                expect(transaction.getResponse()).to.contain('Hello, world!');
+            });
+        });
+    });
 
+    // -------------------------------------------------------------------------
+
+    context('Request with successful redirection', function () {
+        beforeEach(function (done) {
+            transaction = new Transaction({
+                protocol: 'http:',
+                hostname: 'localhost',
+                port: port,
+                method: 'GET',
+                path: '/redirect/302/2'
+            }, undefined, {
+                followRedirects: true,
+                redirectLimit: 10,
+                redirectStatusCodes: [301, 302],
             });
-            it('Emits "response" event', function (done) {
-                setTimeout(function () {
-                    expect(responseListener).called;
-                    done();
-                }, 10);
+            addListeners();
+            transaction.send();
+            setTimeout(done, 10);
+        });
+        describe('Events', function () {
+            it('Emits "request" once', function () {
+                expect(requestListener).calledOnce;
             });
-            it('Emits "end" event', function (done) {
-                setTimeout(function () {
-                    expect(endListener).called;
-                    done();
-                }, 10);
+            it('Emits "response" for each response', function () {
+                expect(responseListener).calledThrice;
             });
-            it('Provides response as string', function (done) {
-                setTimeout(function () {
-                    expect(transaction.getResponse()).to.contain('HTTP/1.1 200 OK');
-                    expect(transaction.getResponse()).to.contain('Hello, world!');
-                    done();
-                }, 10);
+            it('Emits "redirect" for each redirect', function () {
+                expect(redirectListener).calledTwice;
+            });
+            it('Emits "end" once', function () {
+                expect(endListener).calledOnce;
+            });
+            it('Does not emit "error"', function () {
+                expect(errorListener).not.called;
             });
         });
     });
